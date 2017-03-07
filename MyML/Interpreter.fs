f362ec1f5a2c53fc5642057ae85dbb4f169b7e66
pandaman <kointosudesuyo@infoseek.jp>
2017-03-07 23:20:57 +0900

    Added interpreter subs

---
+++ MyML/Interpreter.fs
@@ -0,0 +1,27 @@
+﻿module Interpreter
+
+open Common
+
+type Closure = { name: Var; application: Map<Var,Value> }
+and
+     Value =   Bool of bool
+             | Int of int
+             | Fun of Var
+             | Closure of Closure  
+
+type Environment = { stack: Value list }
+with
+    member this.Push value = 
+        { stack = value :: this.stack }
+    member this.Pop =
+        match this.stack with
+        | [] -> failwith "empty stack"
+        | v :: vs -> v,{ stack = vs }
+
+let rec evalExpr (expr: TypeInference.TypedExpr) (env: Environment): Environment =
+    match expr.value with
+    | TypeInference.Literal(x) -> env.Push (Int(x))
+    | TypeInference.Apply(f,x) -> 
+        let env = evalExpr f env
+        let env = evalExpr x env
+        failwith "???"
